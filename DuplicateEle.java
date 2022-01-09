437218f9e2cef47b7c678a7884d85025aafe4280
Abjeet <abjeets50@gmail.com>
2022-01-10 02:24:30 +0530

    feat: duplicate element program added bitwise operator

---
+++ DuplicateEle.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class DuplicateEle{
+    static byte[] num = new byte[10];
+
+    public static byte Duplicate(byte[] num){
+        byte ans = 0;
+        for (byte i=0;i<9;++i)
+            ans = (byte)(ans ^ num[i]);
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        for (byte i=0; i<9; i++){
+            byte temp = scn.nextByte();
+            num[i] = temp;
+        }
+
+        for (byte x :num)
+            System.out.println(x);
+
+        byte ans = Duplicate(num);
+
+        System.out.println("the answer is" + ans);
+    }
+}
