c23a8e095a46f0d69b1171feb3b73a3013fcb898
Abjeet <abjeets50@gmail.com>
2022-01-10 01:01:06 +0530

    add java program

---
+++ BitEvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class BitEvenOdd{
+    static byte num1;
+
+    public static void main(String[] arg){
+        Scanner scn = new Scanner(System.in);
+        num1 = scn.nextByte();
+
+        if((num1 & 1) == 0)
+            System.out.println(num1 +"is even");
+        else
+            System.out.println(num1 + "is odd");
+    }
+}
