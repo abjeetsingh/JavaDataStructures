6b96eb33df8a5d6b13a9c64b1a8903bd31a2722a
Abjeet <abjeets50@gmail.com>
2022-01-15 00:56:50 +0530

    feat: added reverseString program

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class ReverseString{
+    
+
+    public static String reverseingString(String s, int n){
+        if(n==-1)
+            return "";
+        
+        return (s.charAt(n) + reverseingString(s, n-1)); 
+    }
+    public static void main(String[] args){
+        String s;
+        int n;
+        Scanner scn = new Scanner(System.in);
+
+        s = scn.nextLine();
+        n = s.length();
+        String ans = reverseingString(s, n-1);
+        System.out.println(ans);
+    }
+}
