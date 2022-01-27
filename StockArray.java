26e28e5dd5424f64a5b6b03b0a74318dd1b789f1
Abjeet <abjeets50@gmail.com>
2022-01-27 23:45:59 +0530

    docs: modifies

--- StockArray.java
+++ StockArray.java
@@ -2,22 +2,22 @@
 
 class StockArray{
     public static void main(String[] args) {
-        int arr[] = new int[]{5,3,1,7,4,9,3};
+        int arr[] = new int[]{7,1,5,3,6,4};
         find(arr);
     
     }
 
     public static void find(int[] arr){
-    int minElePos=-1, minEle=arr[0], maxEle;
+    int maxElePos=-1, minEle, maxEle=arr[0];
     for(int i=0; i<arr.length;++i)
-        if(arr[i]<minEle){
-            minEle = arr[i];
-            minElePos = i;
-        }
-    maxEle = arr[minElePos];
-    for(int i=minElePos; i<arr.length;++i)
-        if(arr[i]>maxEle)
+        if(arr[i]>=maxEle){
             maxEle = arr[i];
+            maxElePos = i;
+        }
+    minEle = arr[maxElePos];
+    for(int i=0; i<maxElePos;++i)
+        if(arr[i]<minEle)
+            minEle = arr[i];
     System.out.println("Buy time at --> "+minEle+"\n Sell Time at --> "+maxEle);
     System.out.println("MAX PROFIT --> " + (maxEle-minEle) );
     }
