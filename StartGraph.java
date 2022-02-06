2a6e5918a498604d15d85b739111006bd5696922
Abjeet <abjeets50@gmail.com>
2022-02-06 20:55:48 +0530

    doc: Changes

--- StartGraph.java
+++ StartGraph.java
@@ -17,12 +17,37 @@ class StartGraph{
 
     public static void main(String[] args) {
         int noVer = 7;
-        ArrayList<Edge> graph[] = new ArrayList[7];
+        ArrayList<Edge> graph[] = new ArrayList[7]; // this is an array of arrayList
 
         for(int i=0; i<noVer; ++i)
             graph[i] = new ArrayList<Edge>();
+// LISTING ALL THE EDGES
+        graph[0].add(new Edge(0, 3, 40));
+        graph[0].add(new Edge(0, 1, 10));
 
-        graph[0].add(new Edge(0, 2, 10));
+        graph[0].add(new Edge(1, 0, 10));
+        graph[0].add(new Edge(1, 2, 10));
+
+        graph[0].add(new Edge(2, 3, 10));
+        graph[0].add(new Edge(2, 1, 10));
+
+        graph[0].add(new Edge(3, 0, 40));
+        graph[0].add(new Edge(3, 2, 10));
+        graph[0].add(new Edge(3, 4, 2));
+
+        graph[0].add(new Edge(4, 3, 2));
+        graph[0].add(new Edge(4, 5, 3));
+        graph[0].add(new Edge(4, 6, 3));
+        
+        graph[0].add(new Edge(5, 4, 3));
+        graph[0].add(new Edge(5, 6, 3));
+        
+        graph[0].add(new Edge(6, 5, 3));
+        graph[0].add(new Edge(6, 4, 8));
+        
+        for (ArrayList a:graph)
+        for (Edge edg: a)
+            System.out.println(edg);
     }
-
+    
 }
