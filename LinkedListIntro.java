bc348e1aeeb22cee1ca51fef1de27819d839864c
Abjeet <abjeets50@gmail.com>
2022-02-20 13:38:44 +0530

    feat: remove first added

--- LinkedListIntro.java
+++ LinkedListIntro.java
@@ -25,7 +25,35 @@ public class LinkedlistIntro{
             }
             size++;
         }
-    }
+        void Display(){
+            Node temp = new Node();
+            temp = tail;
+            int i=0;
+            while(temp.next != null){
+                System.out.println("Record"+i+"is value = "+temp.data);
+                ++i;
+            }
+        }
 
+        int removeFirst(){
+            if(head == tail)
+                System.out.println("UNDERFLOW ERROR");
+            else if(size == 1){
+                int removerElement = head.data;
+                temp = head = null;
+                size--;
+            }
+            else{
+                Node temp  = new Node();
+                while(temp.next != head)
+                    temp = temp.next;
+                int removerElement = head.data;
+                head = temp;
+                size --;
+                return removerElement;
+            }
+        }
+    }
+    
     
 }
